7caa522872b500d7a30bb9e2a3272bbadac9332e
Cesar Villegas U <cvillegas@gmail.com>
2020-10-03 08:47:33 -0500

    Monte Carlo sims

---
+++ gen_normal_distribution_numbers.R
@@ -0,0 +1,16 @@
+# define x as male heights from dslabs data
+library(tidyverse)
+library(dslabs)
+data(heights)
+x <- heights %>% filter(sex=="Male") %>% pull(height)
+
+# generate simulated height data using normal distribution - both datasets should have n observations
+n <- length(x)
+avg <- mean(x)
+s <- sd(x)
+simulated_heights <- rnorm(n, avg, s)
+
+# plot distribution of simulated_heights
+data.frame(simulated_heights = simulated_heights) %>%
+  ggplot(aes(simulated_heights)) +
+  geom_histogram(color="black", binwidth = 2)
---
+++ monte_carlo_tallest_person.R
@@ -0,0 +1,6 @@
+B <- 10000
+tallest <- replicate(B, {
+  simulated_data <- rnorm(800, avg, s)    # generate 800 normally distributed random heights
+  max(simulated_data)    # determine the tallest height
+})
+mean(tallest >= 7*12)    # proportion of times that tallest person exceeded 7 feet (84 inches)
